12f49bbe12a9bbec73260ef22872910fa165c698
ricky1122alonefe <zhangrui@ffpms.com>
2020-09-21 18:06:02 +0800

    更新：config 结构体

--- src/conf/config.go
+++ src/conf/config.go
@@ -1,1 +1,16 @@
 package conf
+
+type CananConfig struct {
+	// 访问数据库地址
+	Address string
+	// 用户名
+	UserName string
+	//  密码
+	Password string
+	// schema - db_table list
+	SchemaInfo map[string][]string
+	// 延迟写入
+	Delay int
+	// 写入 方向
+}
+
